f0d174774db36c48161d9cb901b4723bc0f4ac4e
Caroline E <caroline.esquivel01@gmail.com>
2021-05-01 19:11:47 -0500

    created test for shopping cart

---
+++ src/main/java/com/shoppingcart/shoppingcart/tests/ShoppingCartTests.java
@@ -0,0 +1,30 @@
+package com.shoppingcart.shoppingcart.tests;
+
+import com.shoppingcart.shoppingcart.models.Customer;
+import com.shoppingcart.shoppingcart.models.Product;
+import com.shoppingcart.shoppingcart.repositories.CartItemRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.context.TestComponent;
+import org.springframework.test.annotation.Rollback;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(false)
+public class ShoppingCartTests {
+
+    @Autowired
+    private CartItemRepository cartRep;
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Test
+    private void testAddOneCartItem() {
+        Product product = entityManager.find(Product.class, 20);
+        Customer customer = entityManager.find(Customer.class, 5);
+    }
+}
